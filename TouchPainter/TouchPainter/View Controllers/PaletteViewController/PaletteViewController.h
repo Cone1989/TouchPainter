059730b0c8310e041d97c1b6b8527ca605ee45bc
hxsd <hxsd@hxsddeiMac.local>
2018-07-18 10:55:25 +0800

    初始化工程，创建三个主控制器

---
+++ TouchPainter/TouchPainter/View Controllers/PaletteViewController/PaletteViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaletteViewController.h
+//  TouchPainter
+//
+//  Created by hxsd on 2018/7/18.
+//  Copyright © 2018年 hxsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaletteViewController : UIViewController
+
+@end
---
+++ TouchPainter/TouchPainter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchPainter
+//
+//  Created by hxsd on 2018/7/18.
+//  Copyright © 2018年 hxsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
